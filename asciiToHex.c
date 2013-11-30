f461955aeb4bca0f526d8889f0b2a18fdd3284d7
Mark Bowden <Mbowden01@gmail.com>
2013-11-30 16:26:30 +0900

    Update asciiToHex.c

--- asciiToHex.c
+++ asciiToHex.c
@@ -11,3 +11,16 @@ int asToHex(char input){//takes an ASCII value REPRESENTING HEX and puts it into
 {
   return int((input > 0x60? input - 0x57 : input - 0x30));
 }
+
+
+int* makeArray(string input)//makes string representing hex into an array of those values byte by byte
+{
+    int* result = new int[input.length() / 2];
+    for(unsigned int x = 0; x < input.length() / 2; x += 1)
+    {
+        int high = asciiToHex(input[x*2]);
+        int low = asciiToHex(input[x*2+1]);
+        result[x] = (high * 0x10) + low;
+    }
+    return result;
+}

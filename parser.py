6abe1e237619ddd6bcfc0a7308de16496e163b62
Mark Bowden <Mbowden01@gmail.com>
2013-12-19 09:24:38 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,37 +3,14 @@
 import sys
 import string
 
-#1   ()       Brackets
-#2   -        Unary minus
-#3   ^        Exponent
-#4   *, /     Multiply, Divide (left-to-right precedence)
-#5   +, -     Add, Subtract (left-to-right precedence)
+#1 () Brackets
+#2 - Unary minus
+#3 ^ Exponent
+#4 *, / Multiply, Divide (left-to-right precedence)
+#5 +, - Add, Subtract (left-to-right precedence)
 
-# ((-2 + 3) * -3 + 5)
 
-# Make a tree like this
-#                               (   )
-#                               /
-#                              +
-#                             / \ 
-#                            *   5
-#                           / \
-#                         ( ) -3   
-#                         /      
-#                        +   
-#                       / \     
-#                     -2   3  
-#                     
-# If we just add, backing up at ')' we get this 
-#                               R
-#                             ( )
-#                            /  \
-#                           ()  * 
-#                          -   -
-#                         2   3
-#                        +   +
-#                       5   5
-
+# I started with a tree based approach like this
 
 class Node(object):
     def __init__(self):
@@ -104,7 +81,7 @@ class Node(object):
 
     def printAll(self):
         """prints the subtree from node from left"""
-        if self.data:        
+        if self.data:
             print self.data
         if self.left:
             self.left.getTree()
@@ -123,14 +100,9 @@ class Node(object):
         return result
 
 DIGITS = string.digits + '.'
+OPERATORS = '^*/+-'#here we leave out () to proc seperately
 
-def operatorUp( pos ):
-    if pos.isRightChild():
-        parleft = pos.parent.left
-        parleft.remove()
-        parleft.appendTo(pos)
-
-def lex( s ):
+def old_lex( s ):
     """ add things to the tree, only process parens """
     root = Node()
     pos = root
@@ -148,7 +120,7 @@ def lex( s ):
             temp = ""
         if x == ' ':
             continue
-        if x in '^*-+':
+        if x in OPERATORS:
             pos = pos.addChild(x)
         if x == '(':
             pos = pos.addChild('(')
@@ -166,18 +138,100 @@ def lex( s ):
     return root.left
 
 
+#
+#
+#
+# but I have a list based solution that should work and is easier to code
+# for this example
 
-a = lex("2345345-345331")
-[[2345345.0, ['-', [345331.0]]]]
-print a.getTree()
+DIGITS = string.digits + '.'
+OPERATORS = '()^*/+-'
+
+def lex( s ):
+    """ add things to the tree, only process parens """
+    result = []
+    #temp for digits
+    temp = ""
+    for x in s:
+        # Is there any way to make reading digits more straightforward while forloop is for x in s?
+        if x in DIGITS:
+            temp += x
+            continue
+        # first not in digits and there are digits to add
+        elif temp:
+            # but this way we have to flush at the end to catch a digit at the end
+            result.append(float(temp))
+            temp = ""
+        if x == ' ':
+            continue
+        if x in OPERATORS:
+            result.append(x)
+    if temp:
+        result.append(float(temp))
+    # root always only has a left child so do we need it?
+    return result
+
+def brute_parce( line ): 
+
+    # process unary minus
+    for x in range(len(line)):
+        # error to check if float in string although I'd like it to return False here... could write function that does that
+        if line[x] == '-' and (x == 0 or type(line[x - 1]) != float):
+            line[x+1] = -line[x+1]
+            line[x] = None
+
+    line = [x for x in line if x != None]
+
+    # process exponent
+    for x in range(len(line)):
+        if line[x] == '^':
+            line[x+1] = line[x-1] ** line[x+1]
+            line[x], line[x-1] = None, None
+
+    line = [x for x in line if x != None]
+
+    # proccess */
+    for x in range(len(line)):
+        if line[x] == '*':
+            print line
+            line[x+1] = line[x-1] * line[x+1]
+            line[x], line[x-1] = None, None
+        elif line[x] == '/':
+            line[x+1] = line[x-1] / line[x+1]
+            line[x], line[x-1] = None, None
+    
+    line = [x for x in line if x != None]
+    
+    # proccess +-
+    for x in range(len(line)):
+        if line[x] == '+':
+            line[x+1] = line[x-1] + line[x+1]
+            line[x], line[x-1] = None, None
+        elif line[x] == '-':
+            line[x+1] = line[x-1] - line[x+1]
+            line[x], line[x-1] = None, None
+
+    line = [x for x in line if x != None]
+
+    return line
+
+def parse( line ):
+    root = Node()
+    pos = root
+    for x in line:
+    if x == '(':
+            pos = pos.addChild('(')
+            continue
+        if x == ')':
+            pos = pos.parent
+
 a = lex("-2-3")
-print a.getTree()
+print parse(a)
 a = lex("2+3*5")
-print a.getTree()
+print parse(a)
 a = lex("((-2 + 3 + 4) * -3 + 5)")
-
-
-print a.getTree()
+print parse(a)
+#print a
 
 exit()
 

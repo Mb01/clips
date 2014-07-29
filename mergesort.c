c69e577c08b4f9e347841036861bede820882a43
Mark Bowden <Mbowden01@gmail.com>
2014-07-29 13:24:02 +0900

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -4,6 +4,8 @@
 #include "list.h"
 
 /* returns pointer to head of sorted list */
+/* it might be simpler to have a reference to left and set that to head at the end */
+/* e.g. void list_merge(node** head){ left = *head; *head = NULL; ... continue in the same manner but don't return
 node* list_merge_sort(node* left){
 
     /* one element by itself is sorted */
@@ -51,8 +53,7 @@ node* list_merge_sort(node* left){
         }
         
         if(!head){
-            head = use;
-            pos = head;
+            pos = head = use;
         }
             pos->next = use;
             pos = use;

919b561c58ce969db96fd503f4968b594b299828
Mark Bowden <Mbowden01@gmail.com>
2014-08-08 00:54:44 -0500

    add license

--- mergesort.c
+++ mergesort.c
@@ -1,4 +1,16 @@
-/* a linked list implementation */
+/* an in-place mergesort implementation for linked lists */
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    For a copy of the GNU General Public License see http://www.gnu.org/licenses/
+    Copyright Mark Bowden 2014
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include "list.h" /* typedef struct node node | filetolist(char* filename) | list_print(node* head) */
